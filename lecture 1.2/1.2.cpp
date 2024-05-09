55b9d72ef73f6ce462762f584b25c7a49ca186bc
iamsmitmaiyani <smitmaiyani070@gmail.com>
2024-05-09 14:59:42 +0530

    Add files via upload

---
+++ lecture 1.2/1.2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	
+	cout << "Enter the value a:";
+	cin >> a;
+	
+	
+	if(a==2)
+	{
+		cout << "This even number..." << endl;
+	}
+	
+	else
+	{
+			cout << "This odd number..." << endl;
+	}
+}
---
+++ lecture 1.2/1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+ 	 int a,b;
+ 	
+ 	cout << "Enter a first number:" << endl;
+    cout << "Enter a second number:" << endl ;
+    cout << "Enter a third number:" << endl;
+    cin >> a;
+
+ 	
+  if(a<b)
+  {
+       cout << "the given value is minimum";
+  }
+  
+  else
+	 {
+	 	cout << "the given value is maximum";
+	 }
+ 	
+ 	
+ }
