ce8a11a40738703d96bec981270315e875991f4d
iamsmitmaiyani <smitmaiyani070@gmail.com>
2024-05-24 15:26:15 +0530

    Add files via upload

---
+++ pr.3 temperate/sum1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int i,n,f,l,sum;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	l = n%10;
+	while(n>=10)
+	{
+		n /= 10;
+	}
+	f=n;
+	sum =l + f;
+	printf("Sum of two digit is =%d",sum);
+}
